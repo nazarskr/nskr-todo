429f5cf92d10750b79dc736cecb99ef5e3d08937
Skrypnyk <nazarskrypnyk@waterford.org>
2024-12-20 04:35:23 +0000

    build error

---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: false,
+  firebaseConfig: {
+    apiKey: 'YOUR_API_KEY',
+    appId: 'YOUR_APP_ID',
+    authDomain: 'YOUR_AUTH_DOMAIN',
+    messagingSenderId: 'YOUR_MESSAGING_SENDER_ID',
+    projectId: 'YOUR_PROJECT_ID',
+    storageBucket: 'YOUR_STORAGE_BUCKET',
+  },
+};
